bdba5b94474c98a080bd6007029b190f863827f2
Srisrinivasa Penjerla <srisrinivasa.p@HCL.COM>
2018-04-06 12:53:12 +0530

    Initial commit

---
+++ UI/src/app/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NavigationBar.css';
+import { Link, IndexLink } from 'react-router';
+
+const NavBar = (props) => {
+    let navBarSection =
+        <section id="navBar-section">
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header" id="navBarHeading">
+                        <span>Student Enrollment</span>
+                    </div>
+                </div>
+            </nav>
+        </section>;
+
+    return navBarSection;
+  };
+
+export default NavBar;
---
+++ UI/src/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import App from './container/App';
+import EnrollPage from './container/EnrollPage/EnrollPage';
+import './index.css';
+
+render(
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App} >
+                <IndexRoute component={EnrollPage} />
+            </Route>
+        </Router>
+    </Provider>, window.document.getElementById('app'));
