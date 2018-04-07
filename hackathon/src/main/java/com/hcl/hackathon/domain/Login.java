d8fbd8d3d22618d125f032849255bbdd097445b2
admin <nitintonge2020@gmail.com>
2018-04-07 16:51:46 +0530

    pmd check code changes

--- hackathon/src/main/java/com/hcl/hackathon/domain/Login.java
+++ hackathon/src/main/java/com/hcl/hackathon/domain/Login.java
@@ -1,26 +1,60 @@
 package com.hcl.hackathon.domain;
 
+/**
+ * 
+ * @author admin
+ *
+ */
 public class Login {
 	private String userId;
 	private String password;
 	private String role;
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getUserId() {
 		return userId;
 	}
-	public void setUserId(String userId) {
+	
+	/**
+	 * 
+	 * @param userId
+	 */
+	public void setUserId(final String userId) {
 		this.userId = userId;
 	}
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
+	
+	/**
+	 * 
+	 * @param password
+	 */
+	public void setPassword(final String password) {
 		this.password = password;
 	}
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getRole() {
 		return role;
 	}
-	public void setRole(String role) {
+	
+	/**
+	 * 
+	 * @param role
+	 */
+	public void setRole(final String role) {
 		this.role = role;
 	}
 	
